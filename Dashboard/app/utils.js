5b30528b4fea8066441730a628b8d441b427b9ff
AdriSolid <biotekgis@gmail.com>
2019-05-26 18:37:43 +0200

    Add dashboard :bar_chart:

---
+++ Dashboard/app/utils.js
@@ -0,0 +1,7 @@
+define(require => {  
+    return {
+        fillIndicator: (id, value) => document.querySelector(`#${id}-indicator-value`).innerHTML = value ? value.toString().replace('.', ',') : 'No data',
+        fillChart: (instance, newData) => instance.update(newData),
+        flyTo: (mapInstance, coords) => mapInstance.flyTo({ center: [coords.lon, coords.lat ] })
+    }
+});
