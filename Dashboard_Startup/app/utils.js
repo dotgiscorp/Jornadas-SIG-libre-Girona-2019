f6d512a100c83c192017a7f00fa761c315440113
AdriSolid <biotekgis@gmail.com>
2019-05-29 09:18:55 +0200

    Update utils.js

--- Dashboard_Startup/app/utils.js
+++ Dashboard_Startup/app/utils.js
@@ -1,7 +1,3 @@
 define(require => {  
-    return {
-        fillIndicator: (id, value) => document.querySelector(`#${id}-indicator-value`).innerHTML = value ? value.toString().replace('.', ',') : 'No data',
-        fillChart: (instance, newData) => instance.update(newData),
-        flyTo: (mapInstance, coords) => mapInstance.flyTo({ center: [coords.lon, coords.lat ] })
-    }
+    return {}
 });
